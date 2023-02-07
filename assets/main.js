f52807dbe92597c7c7e4d1a1d6222f245bc42c34
Lucas-Buscarioli <lucas.buscarioli@gmail.com>
2023-02-07 17:17:12 -0300

    animando entrada do popup no load da pág

--- assets/main.js
+++ assets/main.js
@@ -27,3 +27,14 @@ const swiper = new Swiper('.swiper', {
       document.body.classList.toggle("dark-mode");
       newsBoxP.classList.toggle("dark-mode");
   })
+
+  //cookies popup 
+
+  const cookieWrap = document.querySelector('.cookies__wrapper'),
+    buttons = document.querySelectorAll ('.cookie__button')
+
+  function execCode () {
+    cookieWrap.classList.add('show');
+  }
+  
+  window.addEventListener('load', execCode);
