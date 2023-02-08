85ca8f6855c285f024cec65efb22495160aaed14
Lucas-Buscarioli <lucas.buscarioli@gmail.com>
2023-02-08 15:22:22 -0300

    colocando elementos e estilizando o meu carrossel

--- assets/main.js
+++ assets/main.js
@@ -6,11 +6,6 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
     spaceBetween: 30,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
